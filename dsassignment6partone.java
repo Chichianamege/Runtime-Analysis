64274fd628857315f18bb116bebcf1563f4ef31a
jake <130408715+Chichianamege@users.noreply.github.com>
2024-05-15 21:54:52 -0400

    Add files via upload
    1.	Use Big O notation to estimate the time complexity of each of the following code fragments. Clearly show all steps of your work. Review examples in textbook pp. 822-827
    
    1.1	sum = 0;
    
    for (int i = 0; i < n; i++){
    sum++;
    
    }
    
    
    
    
    1.2	sum = 0;
    
    for (int i = 0; i < n; i++)
    {
    for (int j = 0; j < n; j++){
    sum++;
    }
    }
    
    
    
    
    1.3	sum = 0;
    
    for (int i = 0; i < n; i++){
    for (int j = 0; j < n * n; j++) {
    sum++;
    }
    
    }
     
    1.4	sum = 0;
    
    for (int i = 0; i < n; i++)  {
    for (int j = 0; j < i; j++) {
    sum++;
    }
    }
    
    1.5	sum 0;
    
    for (int i = 0; i < n; i++)	{
    for (int j = 0; j < i * i; j++) {
    for (int k = 0; k < j; k++) {
    sum++;
    }
    }
    }

---
+++ dsassignment6partone.java
@@ -0,0 +1,16 @@
+// Name: Chidera Anamege
+// Class: CS 3305
+// Term: Spring 2024
+// Instructor: Carla McManus
+// Assignment: 06-Part-2.1-Time
+
+import java.util.Date;
+
+public class dsassignment6partone {
+
+    public static void main(String[] args) {
+        // this will get the current time
+        Date currentTime = new Date();
+        System.out.println("Current Time: " + currentTime);
+    }
+}
